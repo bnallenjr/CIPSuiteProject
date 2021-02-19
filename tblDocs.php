7481799f49ccbe504f08a1475564ce646336beab
bnallenjr <38666330+bnallenjr@users.noreply.github.com>
2021-02-19 14:24:38 -0500

    doc table

---
+++ tblDocs.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+  <div class="table-responsive-sm table-hover table-bordered">          
+  <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th>DocID</th>
+        <th>Title</th>
+        <th>Type</th>
+        <th>Eff. Date</th>
+        <th>Last Review</th>
+		<th>Ver.</th>
+		<th></th>
+      </tr>
+    </thead>
+    <tbody>
+		<tr>
+        <td>CIP-POL-101</td>
+        <td>CIP Cyber Security</td>
+        <td>Policy</td>
+        <td>06/17/2020</td>
+		<td>05/26/2020</td>
+    <td>6.2</td>
+    <td><a href="#">
+          <span class="glyphicon glyphicon-paperclip"></span>
+        </a></td>
+      </tr>
+      <tr>
+        <td>CIP-POL-201</td>
+        <td>Low Impact Cyber Security</td>
+        <td>Policy</td>
+        <td>06/17/2020</td>
+		<td>05/26/2020</td>
+    <td>3.4</td>
+    <td><a href="#">
+          <span class="glyphicon glyphicon-paperclip"></span>
+        </a></td>
+      </tr>
+	  <tr>
+        <td>CIP-PRO-201</td>
+        <td>Asset Management</td>
+        <td>Process</td>
+        <td>06/17/2020</td>
+		<td>05/26/2020</td>
+    <td>5.1</td>
+    <td><a href="#">
+          <span class="glyphicon glyphicon-paperclip"></span>
+        </a></td>
+      </tr>
+	  <tr>
+        <td>CIP-PRC-602</td>
+        <td>Visitor Management</td>
+        <td>Procedure</td>
+        <td>06/17/2020</td>
+		<td>05/26/2020</td>
+    <td>2.5</td>
+    <td><a href="#">
+          <span class="glyphicon glyphicon-paperclip"></span>
+        </a></td>
+      </tr>
+      <tr>
+        <td>CIP-QRF-602</td>
+        <td>Visitor Logging Application</td>
+        <td>Quick Reference Guide</td>
+        <td>06/17/2020</td>
+		<td>05/26/2020</td>
+    <td>6.0</td>
+    <td><a href="#">
+          <span class="glyphicon glyphicon-paperclip"></span>
+        </a></td>
+      </tr>
+    </tbody>
+  </table>
+  </div>
+  <button type="" class="btn btn-primary">Report</button> <button type="" class="btn btn-success">Submit for Approval</button>
+</div>
+
+</body>
+</html>
