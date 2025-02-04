f93f72da611733929e16c1aafcbf512ebfb6b181
bnallenjr <38666330+bnallenjr@users.noreply.github.com>
2025-02-04 14:02:46 -0500

    refractored code

--- index.php
+++ index.php
@@ -1,232 +1,62 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
   <title>CIP SUITE</title>
- 
 </head>
 <body>
-<?php include 'nav.html'?>
-	<div id="accordion">
-    <div class="card">
-      <div class="card-header">
-        <h4>
-          <a class="card-link" data-toggle="collapse" href="#collapse1">Asset Management - (CIP-002)</a>
-        </h4>
-      </div>
-      <div id="collapse1" class="collapse" data-parent="#accordion">
-			<div class="card-body">
-				<ul class="list-group">
-					<button type="button" class="list-group-item list-group-item-action">Documentation</button>
-					<button type="button" class="list-group-item list-group-item-action">Evidence</button>
-					<button type="button" class="list-group-item list-group-item-action">Standard Owner - Olivia Allen</button>
-				</ul>
-			</div>
-      </div>
-    </div>
-	<div class="card">
-      <div class="card-header">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">Cyber Security Program - (CIP-003)</a>
-        </h4>
-      </div>
-      <div id="collapse2" class="collapse" data-parent="#accordion">
-			<div class="panel-body">
-				<ul class="list-group">
-					<button type="button" class="list-group-item list-group-item-action">Documentation</button>
-					<button type="button" class="list-group-item list-group-item-action">Evidence</button>
-					<button type="button" class="list-group-item list-group-item-action">Standard Owner</button>
-				</ul>
-			</div>
-      </div>
-    </div>
-	<div class="card">
-      <div class="card-header">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">Personnel Management - (CIP-004)</a>
-        </h4>
-      </div>
-      <div id="collapse3" class="collapse" data-parent="#accordion">
-			<div class="panel-body">
-				<ul class="list-group">
-					<button type="button" class="list-group-item list-group-item-action">Documentation</button>
-					<button type="button" class="list-group-item list-group-item-action">Evidence</button>
-					<button type="button" class="list-group-item list-group-item-action">Standard Owner</button>
-				</ul>
-			</div>
-      </div>
-    </div>
-	<div class="card">
-      <div class="card-header">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">Networks and ESPs - (CIP-005)</a>
-        </h4>
-      </div>
-      <div id="collapse4" class="collapse" data-parent="#accordion">
-			<div class="panel-body">
-				<ul class="list-group">
-					<button type="button" class="list-group-item list-group-item-action">Documentation</button>
-					<button type="button" class="list-group-item list-group-item-action">Evidence</button>
-					<button type="button" class="list-group-item list-group-item-action">Standard Owner</button>
-				</ul>
-			</div>
-      </div>
-    </div>
-	<div class="card">
-      <div class="card-header">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">Physical Security - (CIP-006)</a>
-        </h4>
-      </div>
-      <div id="collapse5" class="collapse" data-parent="#accordion">
-			<div class="panel-body">
-				<ul class="list-group">
-					<button type="button" class="list-group-item list-group-item-action">Documentation</button>
-					<button type="button" class="list-group-item list-group-item-action">Evidence</button>
-					<button type="button" class="list-group-item list-group-item-action">Standard Owner</button>
-				</ul>
-			</div>
-      </div>
-    </div>
-	<div class="card">
-      <div class="card-header">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse6">System Security Management - (CIP-007)</a>
-        </h4>
-      </div>
-      <div id="collapse6" class="collapse" data-parent="#accordion">
-			<div class="panel-body">
-				<ul class="list-group">
-					<button type="button" class="list-group-item list-group-item-action">Documentation</button>
-					<button type="button" class="list-group-item list-group-item-action">Evidence</button>
-					<button type="button" class="list-group-item list-group-item-action">Standard Owner</button>
-				</ul>
-			</div>
-      </div>
-    </div>
-	<div class="card">
-      <div class="card-header">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse7">Incident Response - (CIP-008)</a>
-        </h4>
-      </div>
-      <div id="collapse7" class="collapse" data-parent="#accordion">
-			<div class="panel-body">
-				<ul class="list-group">
-					<button type="button" class="list-group-item list-group-item-action">Documentation</button>
-					<button type="button" class="list-group-item list-group-item-action">Evidence</button>
-					<button type="button" class="list-group-item list-group-item-action">Standard Owner</button>
-				</ul>
-			</div>
-      </div>
-    </div>
-	<div class="card">
-      <div class="card-header">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse8">System Recovery - (CIP-009)</a>
-        </h4>
-      </div>
-      <div id="collapse8" class="collapse" data-parent="#accordion">
-			<div class="panel-body">
-				<ul class="list-group">
-					<button type="button" class="list-group-item list-group-item-action">Documentation</button>
-					<button type="button" class="list-group-item list-group-item-action">Evidence</button>
-					<button type="button" class="list-group-item list-group-item-action">Standard Owner</button>
-				</ul>
-			</div>
-      </div>
-    </div>
-	<div class="card">
-      <div class="card-header">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse9">Baselining, Change Management, Vulnerability Management - (CIP-010)</a>
-        </h4>
-      </div>
-      <div id="collapse9" class="collapse" data-parent="#accordion">
-			<div class="panel-body">
-				<ul class="list-group">
-					<button type="button" class="list-group-item list-group-item-action">Documentation</button>
-					<button type="button" class="list-group-item list-group-item-action">Evidence</button>
-					<button type="button" class="list-group-item list-group-item-action">Standard Owner</button>
-				</ul>
-			</div>
-      </div>
-    </div>
-	<div class="card">
-      <div class="card-header">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse10">Information Protection - (CIP-011)</a>
-        </h4>
-      </div>
-      <div id="collapse10" class="collapse" data-parent="#accordion">
-			<div class="panel-body">
-				<ul class="list-group">
-					<button type="button" class="list-group-item list-group-item-action">Documentation</button>
-					<button type="button" class="list-group-item list-group-item-action">Evidence</button>
-					<button type="button" class="list-group-item list-group-item-action">Standard Owner</button>
-				</ul>
-			</div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-header">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse11">Control Center Communications - (CIP-012)</a>
-        </h4>
-      </div>
-      <div id="collapse10" class="collapse" data-parent="#accordion">
-			<div class="panel-body">
-				<ul class="list-group">
-					<button type="button" class="list-group-item list-group-item-action">Documentation</button>
-					<button type="button" class="list-group-item list-group-item-action">Evidence</button>
-					<button type="button" class="list-group-item list-group-item-action">Standard Owner</button>
-				</ul>
-			</div>
-      </div>
-    </div>
-	<div class="card">
-      <div class="card-header">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse12">Supply Chain Mitigation Management - (CIP-013)</a>
-        </h4>
-      </div>
-      <div id="collapse10" class="collapse" data-parent="#accordion">
-			<div class="panel-body">
-				<ul class="list-group">
-					<button type="button" class="list-group-item list-group-item-action">Documentation</button>
-					<button type="button" class="list-group-item list-group-item-action">Evidence</button>
-					<button type="button" class="list-group-item list-group-item-action">Standard Owner</button>
-				</ul>
-			</div>
-      </div>
-    </div>
-	<div class="card">
-      <div class="card-header">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse13">Substation Physical Security - (CIP-014)</a>
-        </h4>
-      </div>
-      <div id="collapse10" class="collapse" data-parent="#accordion">
-			<div class="panel-body">
-				<ul class="list-group">
-					<button type="button" class="list-group-item list-group-item-action">Documentation</button>
-					<button type="button" class="list-group-item list-group-item-action">Evidence</button>
-					<button type="button" class="list-group-item list-group-item-action">Standard Owner</button>
-				</ul>
-			</div>
-      </div>
-    </div>
+  <?php include 'nav.html'; ?>
+  <div id="accordion">
+    <?php
+      $cipSections = [
+        ['title' => 'Asset Management', 'id' => 'CIP-002'],
+        ['title' => 'Cyber Security Program', 'id' => 'CIP-003'],
+        ['title' => 'Personnel Management', 'id' => 'CIP-004'],
+        ['title' => 'Networks and ESPs', 'id' => 'CIP-005'],
+        ['title' => 'Physical Security', 'id' => 'CIP-006'],
+        ['title' => 'System Security Management', 'id' => 'CIP-007'],
+        ['title' => 'Incident Response', 'id' => 'CIP-008'],
+        ['title' => 'System Recovery', 'id' => 'CIP-009'],
+        ['title' => 'Baselining, Change Management, Vulnerability Management', 'id' => 'CIP-010'],
+        ['title' => 'Information Protection', 'id' => 'CIP-011'],
+        ['title' => 'Control Center Communications', 'id' => 'CIP-012'],
+        ['title' => 'Supply Chain Mitigation Management', 'id' => 'CIP-013'],
+        ['title' => 'Substation Physical Security', 'id' => 'CIP-014']
+      ];
+
+      foreach ($cipSections as $index => $section) {
+        echo "<div class='card'>
+                <div class='card-header'>
+                  <h4 class='panel-title'>
+                    <a data-toggle='collapse' href='#collapse{$index}' data-parent='#accordion'>
+                      {$section['title']} - ({$section['id']})
+                    </a>
+                  </h4>
+                </div>
+                <div id='collapse{$index}' class='collapse' data-parent='#accordion'>
+                  <div class='card-body'>
+                    <ul class='list-group'>
+                      <button type='button' class='list-group-item list-group-item-action'>Documentation</button>
+                      <button type='button' class='list-group-item list-group-item-action'>Evidence</button>
+                      <button type='button' class='list-group-item list-group-item-action'>Standard Owner</button>
+                    </ul>
+                  </div>
+                </div>
+              </div>";
+      }
+    ?>
   </div>
+
   <div>
-  © <?php
-    $copyYear = 2018; // Set your website start date
-    $curYear = date('Y'); // Keeps the second year updated
+    &copy; <?php
+      $copyYear = 2018; // Set your website start date
+      $curYear = date('Y'); // Keeps the second year updated
       echo $copyYear . (($copyYear != $curYear) ? '-' . $curYear : '');
-  ?> Copyright. Allen Solutions Group Inc. All Rights Reserved.
+    ?> Copyright. Allen Solutions Group Inc. All Rights Reserved.
   </div>
 </body>
 </html>
