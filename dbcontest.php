cc403cf2416e0a05b8755c462023c12cb4785e1e
bnallenjr <38666330+bnallenjr@users.noreply.github.com>
2025-09-04 16:44:11 -0400

    updated dbconnection test

--- dbcontest.php
+++ dbcontest.php
@@ -1,10 +1,21 @@
-
+/*
+		$serverName = '';
+		$connectionInfo=array('Database'=>'CIP_Patch', 'UID'=>'CIPSuite', 'PWD'=>'!FinalFantasy777!');
+		
+		$conn = sqlsrv_connect($serverName, $connectionInfo);
+		if($conn) {
+			//echo 'Connection established<br />';
+		}else{
+			echo 'Connection failure<br />';
+			die(print_r(sqlsrv_errors(), TRUE));
+		}
+    */
 
 
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:asg-db.database.windows.net,1433; Database = asg-db", "asgdb-admin", "!FinalFantasy777!");
+    $conn = new PDO("sqlsrv:server = tcp:asg-db.database.windows.net,1433; Database = asg-db", "asgdb-admin", "{your_password_here}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -13,7 +24,14 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "asgdb-admin", "pwd" => "!FinalFantasy777!", "Database" => "asg-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "asgdb-admin", "pwd" => "{your_password_here}", "Database" => "asg-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:asg-db.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if($conn) {
+			//echo 'Connection established<br />';
+		}else{
+			echo 'Connection failure<br />';
+			die(print_r(sqlsrv_errors(), TRUE));
+		}
 ?>
