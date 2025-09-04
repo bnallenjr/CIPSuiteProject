d44dbffeded40207c7716eaea2e3325edda333d5
bnallenjr <38666330+bnallenjr@users.noreply.github.com>
2025-09-04 16:37:45 -0400

    dbconnection test

--- dbcontest.php
+++ dbcontest.php
@@ -1,12 +1,19 @@
+
+
+
 <?php
-		$serverName = '';
-		$connectionInfo=array('Database'=>'CIP_Patch', 'UID'=>'CIPSuite', 'PWD'=>'!FinalFantasy777!');
-		
-		$conn = sqlsrv_connect($serverName, $connectionInfo);
-		if($conn) {
-			//echo 'Connection established<br />';
-		}else{
-			echo 'Connection failure<br />';
-			die(print_r(sqlsrv_errors(), TRUE));
-		}
-    ?>
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:asg-db.database.windows.net,1433; Database = asg-db", "asgdb-admin", "!FinalFantasy777!");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "asgdb-admin", "pwd" => "!FinalFantasy777!", "Database" => "asg-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:asg-db.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
